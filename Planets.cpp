59db1df7955e437bb12ff8fd5fe7275a5fd44e32
Thomas Martin <ThomasMartin98@hotmail.com>
2016-11-27 20:45:14 +0000

    Add files via upload
    Moved the planet class to its own file and added some more functions.

---
+++ Planets.cpp
@@ -0,0 +1,64 @@
+#include"Planets.h"
+
+
+
+Planet::Planet(std::string name,int raduis, sf::Vector2i position, sf::Color planetColor, float mass,float initalVelocity) {
+	planetName = name;
+	planetPos = position;
+	planetRadius = raduis;
+	orbitRadius= sqrt(pow((int)planetPos.x, 2) + pow((int)planetPos.y, 2));//Will need to change this when I use eliptical orbits
+	currentAngle = 0;
+	planetColor = planetColor;
+	planetMass = mass;
+	planet.setOrigin(planetRadius, planetRadius);
+	//The radius of the orbit of the sun is 0 and we can't / by 0
+	if (orbitRadius != 0) {
+		currentAngle = acos(planetPos.y / orbitRadius) * 180 / PI;
+}	
+	planet.setPointCount(255);
+	planet.setRadius(planetRadius);
+	planet.setPosition((sf::Vector2f)planetPos);
+	planet.setFillColor( planetColor);
+	planetArray.push_back(*this);
+}
+
+void Planet::update() {
+	 getNetForce();
+	 orbitalPeriod = sqrt(4 * pow(PI, 2) / (G*planetArray[0].planetMass)*pow( orbitRadius, 3))/timewarp;
+	 angularVelocity = (2 * PI) /  orbitalPeriod;
+	 currentAngle +=  angularVelocity;
+
+	if ( currentAngle>360) {
+		 currentAngle = 0;
+	}
+	//netForce = getNetForce();
+	//Update Position
+	 planetPos.x = (cos(currentAngle*PI / 180)* orbitRadius);
+	 planetPos.y = (sin(currentAngle*PI / 180)* orbitRadius);
+	 planet.setPosition((sf::Vector2f) planetPos);
+}
+
+sf::CircleShape Planet::getRenderObjet() {
+	return  planet;
+}
+float Planet::returnMass() {
+	return planetMass;
+}
+float Planet::getNetForce() {
+	float currentForce;
+	for (int planetItr = 0; planetItr < planetArray.size(); planetItr++) {
+		sf::Vector2i currentPlanetPos = planetArray[planetItr].returnPos();
+		float distance = sqrt(pow(currentPlanetPos.x - planetPos.x, 2)+ pow(currentPlanetPos.y- planetPos.y,2));
+		//std::cout <<"Distance from "<<planetName<<" to "<<planetArray[planetItr].returnName()<<" : "<< distance << '\n';
+		currentForce += (G*planetArray[planetItr].returnMass()*planetMass)/pow(distance,2);
+		
+	}
+	return currentForce;
+}
+std::string Planet::returnName() {
+	return planetName;
+}
+//DEBUG ALTHOUGH MIGHT NEED IT FOR COLLISION
+sf::Vector2i Planet::returnPos() {
+	return (planetPos);
+}
---
+++ Planets.h
@@ -0,0 +1,41 @@
+#include<SFML/Graphics.hpp>
+#include<vector>
+#include<iostream>
+#include<math.h>
+#include<sstream>
+//Constants
+const float PI = 3.14;
+const float G = 6.67*pow(10, -11);
+
+
+
+//Planet class
+class Planet {
+private:
+	//Variables
+	int planetRadius, orbitRadius;
+	float currentAngle, planetMass, angularVelocity, orbitalPeriod, netForce,velocity;
+	sf::Color planetColor;
+	sf::Vector2i planetPos;
+	sf::CircleShape planet;
+	//Functions
+	//float getNetForce();
+	std::string planetName;
+	
+public:
+	//Variables
+
+	//Functions
+	Planet(std::string name,int raduis, sf::Vector2i Position, sf::Color planetColor, float mass, float initalVelocity);
+	void update();
+	float getNetForce();
+	float returnMass();
+	std::string returnName();
+	sf::CircleShape getRenderObjet();
+	sf::Vector2i returnPos();
+};
+
+//External Variables
+extern std::vector<Planet> planetArray;
+extern int timewarp;
+
